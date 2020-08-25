a92f68de1007e06757ad5e9f128ab37ea6323875
pepelisto <martinmoran422@gmail.com>
2020-08-24 20:41:49 -0400

    few changes

--- airpnp/views.py
+++ airpnp/views.py
@@ -6,6 +6,7 @@ from slugify import slugify
 from datetime import datetime, timedelta
 from django.db.models import Sum
 
+#api/listings/
 class CreateAndShow(APIView):
     def get(self, request):
         listings = Listing.objects.all()
@@ -36,7 +37,7 @@ class CreateAndShow(APIView):
             return Response(serializer.data)
         except:
             return Response('Data not valid')
-
+#api/listings/<int:pk>/
 class ListingCRUD(APIView):
     def get(self, request, pk):
         listings = Listing.objects.get(pk=pk)
@@ -63,9 +64,10 @@ class ListingCRUD(APIView):
 
     def delete(self, request, pk):
          listings = Listing.objects.get(pk=pk)
+         result = [{'id': listings.id}]
          listings.delete()
-         return Response('Object deleted')
-
+         return Response(result)
+#api/listings/<int:pk>/special-prices
 class SpecialPrice(APIView):
     def get(self, request, pk):
         special_prices = Special_price.objects.filter(listing_id=pk)
@@ -75,12 +77,15 @@ class SpecialPrice(APIView):
     def post(self, request, pk):
         data = request.data
         date = data['date']
+        checkIfExist = Special_price.objects.filter(listing_id=pk, date=date).count()
+        if checkIfExist != 0:
+            return Response('there is already a special price for this date')
         price = data['price']
         newSpecialPrice = Special_price(listing_id=pk, date=date, price=price)
         newSpecialPrice.save()
         serializer = Price_dateSerializer(newSpecialPrice, many=False)
         return Response(serializer.data)
-
+#api/listings/<int:pk>/special-prices/<int:pk2>
 class SpecialPriceDelete(APIView):
     def get(self, request, pk, pk2):
         special_prices = Special_price.objects.get(pk=pk2)
@@ -89,9 +94,10 @@ class SpecialPriceDelete(APIView):
 
     def delete(self, request, pk, pk2):
          special_prices = Special_price.objects.get(pk=pk2)
+         result = [{'id': special_prices.id}]
          special_prices.delete()
-         return Response('Object deleted')
-
+         return Response(result)
+#api/listings/<int:pk>/checkout
 class CheckOut(APIView):
     def post(self, request, pk):
         data = request.data
@@ -120,9 +126,9 @@ class CheckOut(APIView):
         elif nights_count > 7:
             discount = listing.weekly_discount
         else:
-            discount = 1
-        dis = nights_cost*(1 - discount)
-        total = nights_cost * discount + cleaning_fee
+            discount = 0
+        dis = nights_cost * discount
+        total = nights_cost * (1- discount) + cleaning_fee
         result = [{'nights_count': nights_count, 'regular_nights': regular_nights, 'special_nights':special_prices_nights, 'total_regular_price' :total_regular_price,
                    'total_special_prices':total_special_prices, 'total_nights_cost': nights_cost, 'discount': dis, 'cleaning_fee':cleaning_fee,'total':total,
                    }]
